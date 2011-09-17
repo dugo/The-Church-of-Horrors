33e3fd2de1c4b6ce4a774761c75713359399d872
Ruben Dugo <rdugomartin@gmail.com>
2011-09-17 18:54:09 +0200

    first commit

---
+++ thechurch-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dugo/Escritorio/Forjas/TheChurchofHorros/thechurch-env/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.3.1','django-admin.py'
+__requires__ = 'Django==1.3.1'
+import pkg_resources
+pkg_resources.run_script('Django==1.3.1', 'django-admin.py')
