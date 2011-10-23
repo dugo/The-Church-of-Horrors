f9c5a1ddd89b8f6b48c342fdae8874d1b7187a34
Ruben Dugo <rdugomartin@gmail.com>
2011-10-23 23:30:00 +0200

    added rss feed

--- TheChurchofHorrors/apps/blog/urls.py
+++ TheChurchofHorrors/apps/blog/urls.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from django.conf.urls.defaults import *
+from feeds import LatestEntriesFeed
 
 urlpatterns = patterns('blog.views',
 
@@ -9,6 +10,7 @@ urlpatterns = patterns('blog.views',
     url(r'^staff/$', 'staff',name='staff'),
     url(r'^author/(?P<user>\w+)/$', 'author',name='author'),
     url(r'^archive/(?P<year>\d+)/(?P<month>\d+)/$', 'archive',name='archive'),
+    url(r'^feed/$', LatestEntriesFeed(),name='feed'),
     url(r'^(?P<slug>[\w-]+)/$', 'common',name="common"),
     url(r'^(?P<section>[\w-]+)/(?P<subsection>[\w-]+)/$', 'section_subsection',name="section_subsection"),
     #url(r'(?P<section>[\w-]+)/(?P<subsection>[\w-]+)/(?P<entry>[\w-]+)/', 'entry',name="entry"),
