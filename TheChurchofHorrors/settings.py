cab5191236f268566421198f9b724ac56eb4f4a7
Ruben Dugo <rdugomartin@gmail.com>
2011-10-23 19:45:35 +0200

    change /media /static again

--- TheChurchofHorrors/settings.py
+++ TheChurchofHorrors/settings.py
@@ -63,7 +63,8 @@ MEDIA_ROOT = os.path.join(SITE_ROOT,"media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://media.thechurchofhorrors.com/'
+#MEDIA_URL = 'http://media.thechurchofhorrors.com/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -73,7 +74,8 @@ STATIC_ROOT = os.path.join(SITE_ROOT,"static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://static.thechurchofhorrors.com/'
+#STATIC_URL = 'http://static.thechurchofhorrors.com/'
+STATIC_URL = '/static/'
 
 ADMIN_MEDIA_PREFIX = STATIC_URL+'admin/'
 
