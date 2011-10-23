0720b78c8f1222af8bebef418c8bd1503850bd32
Ruben Dugo <rdugomartin@gmail.com>
2011-10-23 17:32:39 +0200

    fixing statics and medias

---
+++ TheChurchofHorrors/site/static/tiny_mce/plugins/embed/langs/da.js
@@ -0,0 +1,9 @@
+tinyMCE.addI18n('da.embed',{
+desc:"Inds\u00E6t/rediger embed kode",
+title:"Embed kode",
+source:"Kilde",
+paste_your_embed_code_here:"S¾t ind embed kode her",
+preview:"Forh\u00E5ndsvisning",
+btn_refresh:"Genopfriske",
+btn_update:"Opdater"
+});
