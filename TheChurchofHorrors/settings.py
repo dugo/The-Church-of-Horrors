983d76af87f15b4f49565091d3ad79c7c36bd376
Ruben Dugo <rdugomartin@gmail.com>
2011-10-24 12:00:57 +0200

    change tinymce media url

--- TheChurchofHorrors/settings.py
+++ TheChurchofHorrors/settings.py
@@ -214,6 +214,7 @@ FILEBROWSER_EXTENSIONS = {
     'Image': ['.jpg','.jpeg','.gif','.png','.tif','.tiff'],
 }
 
+TINYMCE_COMPRESSOR = True
 TINYMCE_JS_URL = '/static/tiny_mce/tiny_mce_src.js'
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': "preview,table,spellchecker,paste,searchreplace,media,embed",
