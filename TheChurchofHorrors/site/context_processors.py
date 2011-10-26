79d094649bc6fb4113be5bab3e5f01e4d3b1bb31
Ruben Dugo <rdugomartin@gmail.com>
2011-10-26 17:57:33 +0200

    change order field to sort field

--- TheChurchofHorrors/site/context_processors.py
+++ TheChurchofHorrors/site/context_processors.py
@@ -2,7 +2,7 @@ def common(request):
     from blog.models import Section,Subsection
     
     sections = Section.objects.all().order_by('name')[:]
-    subsections = Subsection.objects.all().order_by('order')[:]
+    subsections = Subsection.objects.all().order_by('sort')[:]
     
     return {
         'sections': sections,
