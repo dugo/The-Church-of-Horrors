f5c94c30497dcc38a3290397b28fb3489480dbe7
Ruben Dugo <rdugomartin@gmail.com>
2011-11-02 10:59:58 +0100

    added unescape filter

--- TheChurchofHorrors/apps/blog/templatetags/blog.py
+++ TheChurchofHorrors/apps/blog/templatetags/blog.py
@@ -1,9 +1,7 @@
 from django.template.defaultfilters import stringfilter
 from django import template
 from apps.blog.models import Entry
-import re
-
-htmlchars = re.compile(r'&[&^];')
+import re, htmlentitydefs
 
 register = template.Library()
 
@@ -64,10 +62,33 @@ def breadcrumb(path,section=None,subsection=None):
 def remove_linebreaks(value):
     return value.replace('\n','').replace('\r','')
     
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
 @register.filter
 @stringfilter
-def remove_htmlchars(value):
-    return re.sub(htmlchars,'',value)
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 @register.filter
 @stringfilter
