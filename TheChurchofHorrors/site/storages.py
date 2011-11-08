967725afd715aa6f63258aeea5817be6bf208174
Ruben Dugo <rdugomartin@gmail.com>
2011-11-08 03:03:57 +0100

    fixing some bugs

---
+++ TheChurchofHorrors/site/storages.py
@@ -0,0 +1,12 @@
+import unicodedata
+
+from django.core.files.storage import FileSystemStorage
+
+class ASCIIFileSystemStorage(FileSystemStorage):
+    """
+    Convert unicode characters in name to ASCII characters.
+    """
+    def get_valid_name(self, name):
+        name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
+        return super(ASCIIFileSystemStorage, self).get_valid_name(name)
+        
