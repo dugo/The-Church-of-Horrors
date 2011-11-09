09365ef6323901955724d47f84aa230acdda9090
Ruben Dugo <rdugomartin@gmail.com>
2011-11-09 23:22:50 +0100

    adjusting tinymce styles

--- TheChurchofHorrors/settings.py
+++ TheChurchofHorrors/settings.py
@@ -169,7 +169,8 @@ TINYMCE_DEFAULT_CONFIG = {
     "embed_iframe_innerhtml_fallback" : 'This will be placed between the iframe tags',
     "theme_advanced_blockformats":"p",
     "force_br_newlines" : True,
-    "force__newlines" : False,
+    "force_p_newlines" : False,
+    "convert_newlines_to_brs" : True,
     "template_templates" : [ { "title" : "Entrevista", "src" : "/static/thechurch/tinymce_templates/entrevista.html" ,  "description" : "Entrevista"  }, { "title" : u"Artículo", "src" : "/static/thechurch/tinymce_templates/critica.html" ,  "description" : u"Artículo"  }, { "title" : u"Crítica de disco", "src" : "/static/thechurch/tinymce_templates/critica-discos.html" ,  "description" : u"Crítica de disco"  }],   
 
 }
