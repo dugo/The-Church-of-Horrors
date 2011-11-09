18ba002f5d981ca331d3b74f254887dbf2c00c5f
Ruben Dugo <rdugomartin@gmail.com>
2011-11-09 23:04:22 +0100

    changed style and editor options

--- TheChurchofHorrors/settings.py
+++ TheChurchofHorrors/settings.py
@@ -168,6 +168,7 @@ TINYMCE_DEFAULT_CONFIG = {
     "extended_valid_elements" : "iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder]",
     "embed_iframe_innerhtml_fallback" : 'This will be placed between the iframe tags',
     "theme_advanced_blockformats":"p",
+    "force_br_newlines" : True,
     "template_templates" : [ { "title" : "Entrevista", "src" : "/static/thechurch/tinymce_templates/entrevista.html" ,  "description" : "Entrevista"  }, { "title" : u"Artículo", "src" : "/static/thechurch/tinymce_templates/critica.html" ,  "description" : u"Artículo"  }, { "title" : u"Crítica de disco", "src" : "/static/thechurch/tinymce_templates/critica-discos.html" ,  "description" : u"Crítica de disco"  }],   
 
 }
