6d4799645c196a34f43557e51b7dec5d5ba2b923
Ruben Dugo <rdugomartin@gmail.com>
2011-11-10 17:29:59 +0100

    added change passwd email

---
+++ scripts/change_passwd.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# generate a random password
+from random import choice,seed
+import time
+import string
+import sys,os
+
+seed(time.time())
+
+def GenPasswd():
+    chars = string.letters + string.digits
+    newpasswd = ''
+    for i in range(8):
+        newpasswd = newpasswd + choice(chars)
+    return newpasswd
+
+activate = "/var/virtualenvs/thechurch/bin/activate_this.py"
+execfile(activate, dict(__file__=activate))
+
+# Django environment
+PROJECT_PATH = "/var/webapp/SpotMonster"
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","TheChurchofHorrors"))
+
+from django.core.management import setup_environ
+sys.path.insert(0,PROJECT_PATH)
+import settings
+setup_environ(settings)
+
+from django.template.loader import render_to_string
+
+for email in sys.argv[1:]:
+    
+    passwd = GenPasswd()
+    u = User.objects.get(username=email.split('@')[0])
+    u.save()
+    
+    # render template
+    email = render_to_string('emails/first_email.html', { 'user': u , 'passwd':passwd})
+    
+    # write email
+    open(emails[i],"w").write(email)
+
