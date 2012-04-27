219ce54a971ecfddf4af6101833e2e0baa3fd13d
root <root@anibal.example.org>
2012-04-27 22:29:22 +0200

    fix related not published

--- TheChurchofHorrors/apps/blog/models.py
+++ TheChurchofHorrors/apps/blog/models.py
@@ -155,7 +155,7 @@ class Entry(models.Model):
         return self.comments.all()
 
     def get_related(self):
-        return Entry.objects.filter(tags__name__in=self.tags.values_list("name",flat=True).all()[:]).exclude(id=self.id).distinct()[:6]
+        return Entry.objects.filter(tags__name__in=self.tags.values_list("name",flat=True).all()[:]).exclude(id=self.id).exclude(published=False).distinct()[:6]
     
     def n_comments(self):
         return self.comments.all().count()
