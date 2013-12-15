b043aaf90bdd91bfa585c7debb02199236110c03
Ruben Dugo Martin <raubn.zero@gmail.com>
2013-12-15 18:20:04 +0100

    dev

--- TheChurchofHorrors/settings.py
+++ TheChurchofHorrors/settings.py
@@ -60,7 +60,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(OLD_SITE_ROOT,"media")
+MEDIA_ROOT = os.path.join(SITE_ROOT,"media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -72,7 +72,7 @@ MEDIA_URL = 'http://media.thechurchofhorrors.com/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(OLD_SITE_ROOT,"static")
+STATIC_ROOT = os.path.join(SITE_ROOT,"static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
