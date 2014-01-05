e93402f9757dc3250565182e871b36ba66483685
Ruben Dugo Martin <raubn.zero@gmail.com>
2014-01-05 20:03:56 +0100

    cartoon and ilustrator fixs

--- TheChurchofHorrors/apps/blog/models.py
+++ TheChurchofHorrors/apps/blog/models.py
@@ -117,7 +117,7 @@ class Number(models.Model):
         return u"Número %d"%self.number
 
     def get_sitios(self):
-        qs = UserProfile.objects.filter(rols__id__in=(2,),user__entries__id__in=self.entries.filter(published=True,is_cartoon=False,is_editorial=False).values_list("id",flat=True))
+        qs = UserProfile.objects.filter(rols__rol__id=2,user__entries__id__in=self.entries.filter(published=True,is_cartoon=False,is_editorial=False).values_list("id",flat=True))
         qs = list(set(qs))
         random.shuffle(qs)
 
--- TheChurchofHorrors/modules/userprofile/models.py
+++ TheChurchofHorrors/modules/userprofile/models.py
@@ -44,7 +44,7 @@ class UserProfile(models.Model):
     
     @classmethod
     def get_authors(cls):
-        return cls.objects.filter(user__entries__published=True,rols__id=2)
+        return cls.objects.filter(user__entries__published=True,rols__rol__id=2)
 
     @classmethod
     def get_by_rol(self,rol_id):
